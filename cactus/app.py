00056bba9abbf65172439a8bbb024f8ea3553d22
Vinay Rangaswamy <vinay.rangaswamy@philips.com>
2025-06-17 12:14:01 +0530

    Update app.py

--- cactus/app.py
+++ cactus/app.py
@@ -2,6 +2,9 @@
 import django
 import subprocess
 
+# Example of a fake AWS secret access key (for testing secret scanning)
+aws_secret = "AKIA1234567890FAKEKEYEXAMPLE"
+
 def insecure_function(user_input):
     # Command injection vulnerability (compliance issue)
     subprocess.call("echo " + user_input, shell=True)
